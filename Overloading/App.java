5d8974035c8f73e6f3f8585efc8cfbc49c4f3f5a
Ellieka <tmdgpdhkdehd@naver.com>
2023-04-17 22:03:18 +0900

    Create App.java

---
+++ Overloading/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        StandardOutput output = new StandardOutput();
+        output.println(100);
+        output.println("hello");
+        output.println(10.5);
+        output.println(false);
+    }
+}
